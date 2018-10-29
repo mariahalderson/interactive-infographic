cacc2e916597067e6c23d0c215b467a85b75e6b7
Alderson <m_alderson59387@tss-hr420-dm15.local>
2018-10-29 10:39:25 -0400

    fetch working

--- js/fetch.js
+++ js/fetch.js
@@ -1,14 +1,16 @@
 var buttons = document.querySelectorAll(".button"),
     textArea = document.querySelector("#infoText");
 
-    function getText(){
-        console.log("clicked");
+    function getText(e){
+        console.log("clicked" + e.target.id);
 
         fetch('info.json')
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            var currentInfo = e.target.id;
+            console.log(data[currentInfo].info);
+            textArea.innerHTML = "<p>" + data[currentInfo].info + "</p>";
         })
     }
 
-    getText();
+    buttons.forEach(button=>button.addEventListener('click', getText));
